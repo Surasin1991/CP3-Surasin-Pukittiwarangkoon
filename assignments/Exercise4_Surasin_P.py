8f06dbdcdb4c2337979ffebe22e269f8245f2203
Surasin1991 <surashin.p@gmail.com>
2020-05-15 18:13:55 +0700

    added new exercise4

---
+++ assignments/Exercise4_Surasin_P.py
@@ -0,0 +1,11 @@
+FE = 95.5
+GB = 89.3
+IC = 99.99
+CP = 99.99
+
+
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",IC)
+print("Computer Programming :",CP)
