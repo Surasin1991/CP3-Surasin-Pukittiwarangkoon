efe58cdcfd7d25819272060b14ff6327b651aea9
Surasin1991 <surashin.p@gmail.com>
2021-02-10 21:31:57 +0700

    t

---
+++ assignments/Lecture50_Surasin_P.py
@@ -0,0 +1,14 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multipliedNumber(x,y):
+    print(x*y)
+def devideNumber(x,y):
+    print(x/y)
+x = int(input("add x : "))
+y = int(input("add y : "))
+addNumber(x,y)
+minusNumber(x,y)
+multipliedNumber(x,y)
+devideNumber(x,y)
