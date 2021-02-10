34afb0dd0ff754ac9a798590bc13da9859b6f605
Surasin1991 <surashin.p@gmail.com>
2021-02-11 03:55:41 +0700

    Update Lecture53_Surasin_p.py

--- assignments/Lecture53_Surasin_p.py
+++ assignments/Lecture53_Surasin_p.py
@@ -2,5 +2,4 @@ def vatCalculate(totalPrice):
     totalPrice = float(input("enter your price : "))
     result = totalPrice+(totalPrice*7/100)
     return result
-
 print(("Price Include vat = "),vatCalculate(0))
