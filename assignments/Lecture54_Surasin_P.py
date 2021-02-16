468c83447d22ca0e68126b87ef0e571d5b405350
Surasin1991 <65213902+Surasin1991@users.noreply.github.com>
2021-02-16 21:01:28 +0700

    t

---
+++ assignments/Lecture54_Surasin_P.py
@@ -0,0 +1,32 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+def menuSelect():
+    userSelected = int(input("Select menu>> "))
+    return userSelected
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    print("Your total price = ",result)
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+if login():
+    showMenu()
+    userSelected = menuSelect()
+    if userSelected == 1:
+        totalPrice = int(input("Enter your price : "))
+        vatCalculator(totalPrice)
+    elif userSelected == 2:
+        priceCalculator()
+else:
+    print("Username or Password was wrong!")
